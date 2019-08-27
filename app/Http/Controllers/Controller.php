27e5891850106e45dfb00c5526d67dd93b5da067
inaldomonteiroti <inaldomonteiroti@gmail.com>
2019-08-27 09:22:39 -0300

    Inclusão de um package l5 Repository no projeto

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function homepage()
+    {
+    //helper que traz a rendereização de uma view passando variavel para view
+    $variavel = "- Homepage do binvestimento";
+    return view('welcome' ,[
+        'title' => $variavel
+    ]);
+    }
+
+    public function cadastrar()
+    {
+    echo "Fazer um cadastro";
+    }
+
+    public function fazerLogin()
+    {
+    echo "Tela de Login";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Quando vc acessar a tota barra vc vai usar o metodo do controle Controller
+Route::get('/', ['uses' => 'Controller@homepage']);
+Route::get('/cadastro', ['uses' => 'Controller@cadastrar']);
+Route::get('/login', ['uses' => 'Controller@fazerLogin']);
+
+//Essa rota é acessada durante a inicialização do laravel http://127.0.0.1:8000
+// Route::get('/', function () {
+//     return view('welcome');
+// });
